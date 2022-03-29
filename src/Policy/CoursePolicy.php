ec889257456eb9811d19b1fbf75fa82b54c02ee8
patrickakk <patrick@algority.eu>
2022-03-29 22:52:43 +0200

    updated CoursePolicy

--- src/Policy/CoursePolicy.php
+++ src/Policy/CoursePolicy.php
@@ -6,53 +6,45 @@ namespace DhcrCore\Policy;
 use Authorization\IdentityInterface;
 use DhcrCore\Model\Entity\Course;
 
-/**
- * Course policy
- */
 class CoursePolicy
 {
-    /**
-     * Check if $user can add Course
-     *
-     * @param \Authorization\IdentityInterface $user The user.
-     * @param \DhcrCore\Model\Entity\Course $course
-     * @return bool
-     */
     public function canAdd(IdentityInterface $user, Course $course)
     {
         return true;
     }
 
-    /**
-     * Check if $user can edit Course
-     *
-     * @param \Authorization\IdentityInterface $user The user.
-     * @param \DhcrCore\Model\Entity\Course $course
-     * @return bool
-     */
     public function canEdit(IdentityInterface $user, Course $course)
     {
+        if($course->user_id == $user->id) {  // contributor edits own course
+            return true;
+        }
+        if($user->user_role_id == 2 && $course->country_id == $user->country_id) {  //  moderator edits in own country
+            return true;
+        }
+        if($user->is_admin) {  // admin can edit all
+            return true;
+        }
+        return false;
     }
 
-    /**
-     * Check if $user can delete Course
-     *
-     * @param \Authorization\IdentityInterface $user The user.
-     * @param \DhcrCore\Model\Entity\Course $course
-     * @return bool
-     */
     public function canDelete(IdentityInterface $user, Course $course)
     {
+        return false;
     }
 
-    /**
-     * Check if $user can view Course
-     *
-     * @param \Authorization\IdentityInterface $user The user.
-     * @param \DhcrCore\Model\Entity\Course $course
-     * @return bool
-     */
     public function canView(IdentityInterface $user, Course $course)
     {
+        return true;
+    }
+
+    public function canApprove(IdentityInterface $user, Course $course)
+    {
+        if($user->user_role_id == 2 && $course->country_id == $user->country_id) {  //  moderator approves in own country
+            return true;
+        }
+        if($user->is_admin) {  // admin can approve all
+            return true;
+        }
+        return false;
     }
 }
