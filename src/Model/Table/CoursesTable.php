93c4e954083781df2bca05ea13abce5ebaa8acff
patrickakk <patrick@algority.eu>
2022-10-28 13:00:23 +0200

    Revert "update association"
    This reverts commit 57c6ef225e3b27f973936ddc37a29035a06ebc76.

--- src/Model/Table/CoursesTable.php
+++ src/Model/Table/CoursesTable.php
@@ -129,17 +129,17 @@ class CoursesTable extends Table
 			'foreignKey' => 'course_duration_unit_id'
 		]);
 
-		$this->belongsToMany('DhcrCore.Disciplines', [
+		$this->belongsToMany('Disciplines', [
 			'foreignKey' => 'course_id',
 			'targetForeignKey' => 'discipline_id',
 			'joinTable' => 'courses_disciplines',
 		]);
-		$this->belongsToMany('DhcrCore.TadirahObjects', [
+		$this->belongsToMany('TadirahObjects', [
 			'foreignKey' => 'course_id',
 			'targetForeignKey' => 'tadirah_object_id',
 			'joinTable' => 'courses_tadirah_objects',
 		]);
-		$this->belongsToMany('DhcrCore.TadirahTechniques', [
+		$this->belongsToMany('TadirahTechniques', [
 			'foreignKey' => 'course_id',
 			'targetForeignKey' => 'tadirah_technique_id',
 			'joinTable' => 'courses_tadirah_techniques',
