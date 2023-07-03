0fb5816267400acd790fef1c6513fdaeb46a43b0
patrickakk <patrick@algority.eu>
2023-07-03 11:28:48 +0200

    reformat code

--- src/Model/Table/LanguagesTable.php
+++ src/Model/Table/LanguagesTable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DhcrCore\Model\Table;
 
 use Cake\Core\Configure;
@@ -37,7 +38,7 @@ class LanguagesTable extends Table
      * @param array $config The configuration for the Table.
      * @return void
      */
-    public function initialize(array $config) : void
+    public function initialize(array $config): void
     {
         parent::initialize($config);
 
@@ -58,7 +59,7 @@ class LanguagesTable extends Table
      * @param \Cake\Validation\Validator $validator Validator instance.
      * @return \Cake\Validation\Validator
      */
-    public function validationDefault(Validator $validator) : Validator
+    public function validationDefault(Validator $validator): Validator
     {
         $validator
             ->integer('id')
@@ -75,15 +76,17 @@ class LanguagesTable extends Table
 
 
     // entry point for querystring evaluation
-    public function evaluateQuery($requestQuery = array()) {
+    public function evaluateQuery($requestQuery = array())
+    {
         $this->getCleanQuery($requestQuery);
         $this->getFilter();
     }
 
 
-    public function getCleanQuery($query = array()) {
-        foreach($query as $key => $value) {
-            if(!in_array($key, $this->allowedParameters)) {
+    public function getCleanQuery($query = array())
+    {
+        foreach ($query as $key => $value) {
+            if (!in_array($key, $this->allowedParameters)) {
                 unset($query[$key]);
                 continue;
             }
@@ -92,15 +95,16 @@ class LanguagesTable extends Table
     }
 
 
-    public function getFilter() {
-        foreach($this->query as $key => $value) {
-            switch($key) {
+    public function getFilter()
+    {
+        foreach ($this->query as $key => $value) {
+            switch ($key) {
                 case 'sort_count':
                 case 'course_count':
                 case 'count_recent':
-                    if($value == true || $value === '')
+                    if ($value == true || $value === '')
                         $this->query[$key] = true;
-                    if(($key == 'sort_count' OR $key == 'count_recent') AND $this->query[$key])
+                    if (($key == 'sort_count' or $key == 'count_recent') and $this->query[$key])
                         $this->query['course_count'] = true;
                     break;
             }
@@ -109,8 +113,9 @@ class LanguagesTable extends Table
     }
 
 
-    public function getLanguage($id = null) {
-        if(!empty($this->query['count_recent'])) {
+    public function getLanguage($id = null)
+    {
+        if (!empty($this->query['count_recent'])) {
             $this->hasMany('DhcrCore.Courses', [
                 'foreignKey' => 'language_id',
                 'conditions' => [
@@ -123,7 +128,7 @@ class LanguagesTable extends Table
         }
         $record = $this->get($id, [
             'contain' => [],
-            'fields' => ['id','name']
+            'fields' => ['id', 'name']
         ]);
         $record->setVirtual(['course_count']);
         return $record;
@@ -132,8 +137,9 @@ class LanguagesTable extends Table
     /*
      * Due to iterative post-processing, method returns either array of entities or array of arrays!
      */
-    public function getLanguages() {
-        if(!empty($this->query['count_recent'])) {
+    public function getLanguages()
+    {
+        if (!empty($this->query['count_recent'])) {
             $this->hasMany('DhcrCore.Courses', [
                 'foreignKey' => 'language_id',
                 'conditions' => [
@@ -145,18 +151,17 @@ class LanguagesTable extends Table
             ]);
         }
         $records = $this->find()
-            ->select(['id','name'])
+            ->select(['id', 'name'])
             ->contain([])
             ->order(['Languages.name' => 'ASC'])
             ->toArray();
 
-        if(!empty($this->query['course_count']))
-            foreach($records as &$record) $record->setVirtual(['course_count']);
+        if (!empty($this->query['course_count']))
+            foreach ($records as &$record) $record->setVirtual(['course_count']);
         // sort by course_count descending, using CounterSortBehavior
-        if(!empty($this->query['sort_count']))
+        if (!empty($this->query['sort_count']))
             $records = $this->sortByCourseCount($records);
 
         return $records;
     }
-
 }
