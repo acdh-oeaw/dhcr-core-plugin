17acc56c200df3690747e6648cc8a3d16f3b4ec8
patrickakk <patrick@algority.eu>
2024-05-13 16:12:49 +0200

    add policy for Counties

---
+++ src/Policy/CountryPolicy.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace DhcrCore\Policy;
+
+use Authorization\IdentityInterface;
+use DhcrCore\Model\Entity\Country;
+
+class CountryPolicy
+{
+    public function canAdd(IdentityInterface $user, Country $country)
+    {
+        if ($user->is_admin) {
+            return true;
+        }
+        return false;
+    }
+
+    public function canEdit(IdentityInterface $user, Country $country)
+    {
+        if ($user->is_admin) {
+            return true;
+        }
+        return false;
+    }
+}
