b287f09f77333b8f25dd5b1a749329b7eda95397
patrickakk <patrick@algority.eu>
2024-11-15 18:30:30 +0100

    removed default comments

--- src/Model/Entity/Country.php
+++ src/Model/Entity/Country.php
@@ -1,36 +1,14 @@
 <?php
+
 namespace DhcrCore\Model\Entity;
 
 use Cake\ORM\Entity;
 use JeremyHarris\LazyLoad\ORM\LazyLoadEntityTrait;
 
-/**
- * Country Entity
- *
- * @property int $id
- * @property string $name
- * @property string|null $domain_name
- * @property string|null $stop_words
- *
- * @property \App\Model\Entity\City[] $cities
- * @property \App\Model\Entity\Course[] $courses
- * @property \App\Model\Entity\Institution[] $institutions
- * @property \App\Model\Entity\User[] $users
- */
 class Country extends Entity
 {
     use LazyLoadEntityTrait;
 
-
-	/**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
     protected $_accessible = [
         'name' => true,
         'domain_name' => true,
@@ -42,15 +20,16 @@ class Country extends Entity
     ];
 
     protected $_hidden = [
-    	'domain_name',
-		'stop_words',
-		'courses'
-	];
+        'domain_name',
+        'stop_words',
+        'courses'
+    ];
 
     // make virtual fields visible for JSON serialization
-	//protected $_virtual = ['course_count'];
+    //protected $_virtual = ['course_count'];
 
-    protected function _getCourseCount() {
-    	return count($this->courses);
-	}
+    protected function _getCourseCount()
+    {
+        return count($this->courses);
+    }
 }
