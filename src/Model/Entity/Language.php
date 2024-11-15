0d465547a5dbaa0da396da6ca4702b21e10c35d4
patrickakk <patrick@algority.eu>
2024-11-15 19:00:05 +0100

    removed default comments

--- src/Model/Entity/Language.php
+++ src/Model/Entity/Language.php
@@ -1,31 +1,14 @@
 <?php
+
 namespace DhcrCore\Model\Entity;
 
 use Cake\ORM\Entity;
 use JeremyHarris\LazyLoad\ORM\LazyLoadEntityTrait;
 
-/**
- * Language Entity
- *
- * @property int $id
- * @property string $name
- *
- * @property \App\Model\Entity\Course[] $courses
- */
 class Language extends Entity
 {
-
     use LazyLoadEntityTrait;
 
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
     protected $_accessible = [
         'name' => true,
         'courses' => true
@@ -35,8 +18,8 @@ class Language extends Entity
         'courses'
     ];
 
-
-    protected function _getCourseCount() {
+    protected function _getCourseCount()
+    {
         return count($this->courses);
     }
 }
