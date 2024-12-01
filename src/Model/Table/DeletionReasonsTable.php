acb2bff6a8f9c47fb7b84cb72934221f30e1fac3
patrickakk <patrick@algority.eu>
2024-12-01 15:28:45 +0100

    removed default comments

--- src/Model/Table/DeletionReasonsTable.php
+++ src/Model/Table/DeletionReasonsTable.php
@@ -7,28 +7,8 @@ use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
-/**
- * DeletionReasons Model
- *
- * @property \App\Model\Table\CoursesTable|\Cake\ORM\Association\HasMany $Courses
- *
- * @method \App\Model\Entity\DeletionReason get($primaryKey, $options = [])
- * @method \App\Model\Entity\DeletionReason newEntity($data = null, array $options = [])
- * @method \App\Model\Entity\DeletionReason[] newEntities(array $data, array $options = [])
- * @method \App\Model\Entity\DeletionReason|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\DeletionReason saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\DeletionReason patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method \App\Model\Entity\DeletionReason[] patchEntities($entities, array $data, array $options = [])
- * @method \App\Model\Entity\DeletionReason findOrCreate($search, callable $callback = null, $options = [])
- */
 class DeletionReasonsTable extends Table
 {
-    /**
-     * Initialize method
-     *
-     * @param array $config The configuration for the Table.
-     * @return void
-     */
     public function initialize(array $config): void
     {
         parent::initialize($config);
@@ -42,12 +22,6 @@ class DeletionReasonsTable extends Table
         ]);
     }
 
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
     public function validationDefault(Validator $validator): Validator
     {
         $validator
@@ -59,7 +33,6 @@ class DeletionReasonsTable extends Table
             ->maxLength('name', 255)
             ->requirePresence('name', 'create')
             ->allowEmptyString('name', false);
-
         return $validator;
     }
 
