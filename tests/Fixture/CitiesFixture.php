3feab57e8c497e67665a41f3454e8d099c1813b0
patrickakk <patrick@algority.eu>
2024-12-01 21:10:18 +0100

    removed default comments

--- tests/Fixture/CitiesFixture.php
+++ tests/Fixture/CitiesFixture.php
@@ -1,18 +1,11 @@
 <?php
+
 namespace DhcrCore\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
-/**
- * CitiesFixture
- */
 class CitiesFixture extends TestFixture
 {
-    /**
-     * Fields
-     *
-     * @var array
-     */
     // @codingStandardsIgnoreStart
     public $fields = [
         'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
@@ -31,12 +24,8 @@ class CitiesFixture extends TestFixture
         ],
     ];
     // @codingStandardsIgnoreEnd
-    /**
-     * Init method
-     *
-     * @return void
-     */
-    public function init() : void
+
+    public function init(): void
     {
         $this->records = [
             [
