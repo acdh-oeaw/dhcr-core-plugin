4ee6fb07f0212a9a638ef22df574edd456beee97
patrickakk <patrick@algority.eu>
2024-12-01 15:07:44 +0100

    removed default comments

--- src/Model/Table/CountriesTable.php
+++ src/Model/Table/CountriesTable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DhcrCore\Model\Table;
 
 use Cake\ORM\Query;
@@ -7,45 +8,21 @@ use Cake\ORM\Table;
 use Cake\Validation\Validator;
 use Cake\Core\Configure;
 
-/**
- * Countries Model
- *
- * @property \App\Model\Table\CitiesTable|\Cake\ORM\Association\HasMany $Cities
- * @property \App\Model\Table\CoursesTable|\Cake\ORM\Association\HasMany $Courses
- * @property \App\Model\Table\InstitutionsTable|\Cake\ORM\Association\HasMany $Institutions
- *
- * @method \App\Model\Entity\Country get($primaryKey, $options = [])
- * @method \App\Model\Entity\Country newEntity($data = null, array $options = [])
- * @method \App\Model\Entity\Country[] newEntities(array $data, array $options = [])
- * @method \App\Model\Entity\Country|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Country saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Country patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method \App\Model\Entity\Country[] patchEntities($entities, array $data, array $options = [])
- * @method \App\Model\Entity\Country findOrCreate($search, callable $callback = null, $options = [])
- */
 class CountriesTable extends Table
 {
+    public $query = array();
 
-	public $query = array();
-
-	public $allowedParameters = [
-		'course_count',
-		'sort_count',
+    public $allowedParameters = [
+        'course_count',
+        'sort_count',
         'count_recent'
-	];
+    ];
 
-
-	/**
-     * Initialize method
-     *
-     * @param array $config The configuration for the Table.
-     * @return void
-     */
-    public function initialize(array $config) : void
+    public function initialize(array $config): void
     {
         parent::initialize($config);
 
-		$this->addBehavior('DhcrCore.CounterSort');
+        $this->addBehavior('DhcrCore.CounterSort');
 
         $this->setTable('countries');
         $this->setDisplayField('name');
@@ -62,13 +39,7 @@ class CountriesTable extends Table
         ]);
     }
 
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
-    public function validationDefault(Validator $validator) : Validator
+    public function validationDefault(Validator $validator): Validator
     {
         $validator
             ->integer('id')
@@ -98,47 +69,47 @@ class CountriesTable extends Table
         return $validator;
     }
 
+    // entrance point for querystring evaluation
+    public function evaluateQuery($requestQuery = array())
+    {
+        $this->getCleanQuery($requestQuery);
+        $this->getFilter();
+    }
 
-	// entrance point for querystring evaluation
-	public function evaluateQuery($requestQuery = array()) {
-		$this->getCleanQuery($requestQuery);
-		$this->getFilter();
-	}
+    public function getCleanQuery($query = array())
+    {
+        foreach ($query as $key => $value) {
+            if (!in_array($key, $this->allowedParameters)) {
+                unset($query[$key]);
+                continue;
+            }
+        }
+        return $this->query = $query;
+    }
 
-
-	public function getCleanQuery($query = array()) {
-		foreach($query as $key => $value) {
-			if(!in_array($key, $this->allowedParameters)) {
-				unset($query[$key]);
-				continue;
-			}
-		}
-		return $this->query = $query;
-	}
-
-
-	public function getFilter() {
-		foreach($this->query as $key => $value) {
-			switch($key) {
-				case 'sort_count':
-				case 'course_count':
+    public function getFilter()
+    {
+        foreach ($this->query as $key => $value) {
+            switch ($key) {
+                case 'sort_count':
+                case 'course_count':
                 case 'count_recent':
-					if($value == true || $value === '')
-						$this->query[$key] = true;
-                    if(($key == 'sort_count' OR $key == 'count_recent') AND $this->query[$key])
-						$this->query['course_count'] = true;
-			}
-		}
-		return $this->query;
-	}
+                    if ($value == true || $value === '')
+                        $this->query[$key] = true;
+                    if (($key == 'sort_count' or $key == 'count_recent') and $this->query[$key])
+                        $this->query['course_count'] = true;
+            }
+        }
+        return $this->query;
+    }
 
-
-    public function getCountry($id = null) {
-    	$country = $this->get($id, [
-			'contain' => [],
-			'fields' => ['id','name']
-		]);
-        if(!empty($this->query['count_recent'])) {
+    public function getCountry($id = null)
+    {
+        $country = $this->get($id, [
+            'contain' => [],
+            'fields' => ['id', 'name']
+        ]);
+        if (!empty($this->query['count_recent'])) {
             // override with more detailed conditions
             $this->hasMany('DhcrCore.Courses', [
                 'foreignKey' => 'country_id',
@@ -150,15 +121,16 @@ class CountriesTable extends Table
                 ]
             ]);
         }
-    	$country->setVirtual(['course_count']);
-    	return $country;
-	}
+        $country->setVirtual(['course_count']);
+        return $country;
+    }
 
-	/*
+    /*
 	 * Due to iterative post-processing, method returns either array of entities or array of arrays!
 	 */
-	public function getCountries() {
-        if(!empty($this->query['count_recent'])) {
+    public function getCountries()
+    {
+        if (!empty($this->query['count_recent'])) {
             $this->hasMany('DhcrCore.Courses', [
                 'foreignKey' => 'country_id',
                 'conditions' => [
@@ -169,21 +141,17 @@ class CountriesTable extends Table
                 ]
             ]);
         }
-	    $countries = $this->find()
-			->select(['id','name'])
-			->contain([])
-			->order(['Countries.name' => 'ASC'])
-			->toArray();
+        $countries = $this->find()
+            ->select(['id', 'name'])
+            ->contain([])
+            ->order(['Countries.name' => 'ASC'])
+            ->toArray();
 
-        if(!empty($this->query['course_count']))
-            foreach($countries as &$country) $country->setVirtual(['course_count']);
+        if (!empty($this->query['course_count']))
+            foreach ($countries as &$country) $country->setVirtual(['course_count']);
         // sort by course_count descending, using CounterSortBehavior
-        if(!empty($this->query['sort_count']))
+        if (!empty($this->query['sort_count']))
             $countries = $this->sortByCourseCount($countries);
-
-		return $countries;
-	}
-
-
-
+        return $countries;
+    }
 }
